c94ea51f11740fa6092a76f960b4711f23b1b63d
Simon Fabcic <simon.fabcic@gmail.com>
2024-03-05 07:25:44 +0100

    .

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Customer
+
+
+# Define an inline admin descriptor for Customer model
+# which acts a bit like a singleton
+class CustomerInline(admin.StackedInline):
+    model = Customer
+    can_delete = False
+    verbose_name_plural = "customer"
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = [CustomerInline]
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    # https://docs.djangoproject.com/en/5.0/ref/contrib/auth/
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # TODO avatar = 
