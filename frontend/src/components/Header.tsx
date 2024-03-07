e825fc4e21db106aa895378138851824d54aa487
Simon Fabcic <simon.fabcic@gmail.com>
2024-03-07 09:25:59 +0100

    editing header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Link, NavLink } from "react-router-dom"
+import { useContext } from "react"
+import AuthContext from "../pages/context/AuthContext"
 
 const Header = () => {
+
+  let { loggedInUser } = useContext(AuthContext)
+
   return (
     <>
-      <div className="text-3xl font-bold underline">Header</div>
+      <div className="flex justify-between h-8">
+        {/* CONTINUE */}
+        {/* TODO adjust image height */}
+        <Link to="/">
+          <img
+            src="../images/logo.png"
+            alt="skillup mentor logo"
+            // className="w-28"
+          />
+        </Link>
+        <div>
+          <NavLink to="/"> Home </NavLink>
+          <NavLink to="/"> Dashboard </NavLink>
+          {loggedInUser ? (
+            <button type="button">Logout</button>
+          ) : (
+            <>
+              <NavLink to="/"> Login </NavLink>
+              <NavLink to="/"> Register </NavLink>
+            </>
+          )}
+        </div>
+      </div>
       {/* 
         TODO:  Implement a menu bar for the header.
           skillup mentor logo
