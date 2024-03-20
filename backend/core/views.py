b7a1db4eb3385aae3e364329e3b176e35e56b3f8
Simon Fabcic <simon.fabcic@gmail.com>
2024-03-20 13:20:44 +0100

    Added backend authentication

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getTest(request):
+    testData = [
+      {
+        "key": "key1",
+        "label": "label1"
+      },
+      {
+        "key": "key2",
+        "label": "label2"
+      },
+      {
+        "key": "key3",
+        "label": "label3"
+      },
+      {
+        "key": "key4",
+        "label": "label4"
+      }
+    ]
+    return Response({"data":testData})
