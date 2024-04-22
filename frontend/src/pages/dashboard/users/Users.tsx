55c2779419bce0149c57ee6acfe7438a2409f7e6
simonfabcic <63279217+simonfabcic@users.noreply.github.com>
2024-04-22 20:52:09 +0200

    Revert "Added numbers for pagination on the bottom of users table"

--- frontend/src/pages/dashboard/users/Users.tsx
+++ frontend/src/pages/dashboard/users/Users.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import useAxios from "../../utils/useAxios"
 import { useNavigate } from "react-router-dom";
 
@@ -18,13 +18,14 @@ const Users = () => {
     email: string;
     full_name: string;
     role: string;
+    // username: string;
   }
   
   interface ApiResponseProfiles {
     headers: Header[];
     rows: Row[];
     pagination_description: {
-      current_page: number;
+      page: number;
       total_pages: number;
       sort_order: "ASC" | "DESC";
       sort_by: string;
@@ -45,7 +46,7 @@ const Users = () => {
     headers:[],
     rows:[],
     pagination_description:{
-      current_page: 0,
+      page: 0,
       total_pages: 0,
       sort_order: "ASC",
       sort_by: "",
@@ -57,14 +58,15 @@ const Users = () => {
   })
   const [sortingColumn, setSortingColumn] = useState("role")
   const [sortOrder, setSortOrder] = useState<"ASC" | "DESC">("ASC")
+  // const [paginationPage, setPaginationPage] = useState(0)
+  // const [paginationLimit, setPaginationLimit] = useState(20)
   const [loadingState, setLoadingState] = useState("Loading...")
 
   const [deleteButtonPressed, setDeleteButtonPressed] = useState(false)
   const [deleteButtonPressedOnUserId, setDeleteButtonPressedOnUserId] = useState(-1)
-  const [paginationWantedPage, setPaginationWantedPage] = useState(1)
-  const [paginationPageNumbers, setPaginationPageNumbers] = useState<number[]>([])
 
-  const paginationPageSize = 100
+  let paginationPage = 1
+  let paginationPageSize = 5
 
   // API REQUESTS -------------------------------------------------------------
   // --------------------------------------------------------------------------
@@ -72,23 +74,18 @@ const Users = () => {
   // Get all profiles ---------------------------
   useEffect(() => {
     getProfiles()
-  }, [sortingColumn, sortOrder, paginationWantedPage])
+  }, [sortingColumn, sortOrder])
   let getProfiles = async () => {
     try {
-      let response = await api.get((`/shop-api-v1/profiles?offset="0"&page=${paginationWantedPage}&page_size=${paginationPageSize}&sort_by=${sortingColumn}&sort_order=${sortOrder}`))
+      let response = await api.get((`/shop-api-v1/profiles?offset="0"&page=${paginationPage}&page_size=${paginationPageSize}&sort_by=${sortingColumn}&sort_order=${sortOrder}`))
+      // QUESTION how to handle http 400 response message - 'response' value is not accessible in 'catch'
       if (response.status === 200) {
-        setTableData(() => response.data)
+        setTableData(response.data)
         if (response.data.rows.length < 1)
           setLoadingState("Didn't receive any profiles. Nothing to show.")
-        let currentPage = response.data.pagination_description.current_page
-        let startPage = Math.max(1, currentPage - 2)
-        let endPage = Math.min (currentPage + 2, response.data.pagination_description.total_pages)
-        let newNumbers: number[] = []
-        for (let i=startPage;i<=endPage;i++)
-          newNumbers.push(i)
-        setPaginationPageNumbers(newNumbers)
       }
     } catch (err: any) {
+      // console.log("Response: ", err.response)
       console.error("During getting 'Profiles', err occurred: ", err.message, "\nMessage from server:", err.response.data);
       setLoadingState("Can't get profiles data. Contact admin...")
     }
@@ -98,6 +95,7 @@ const Users = () => {
   let deleteProfile = async (id: number) => {
     try {
       let response = await api.delete((`/shop-api-v1/profile/${id}`))
+      //console.log(response)
       if (response.status === 204) {
         setTableData(prevTableState => ({
           headers : prevTableState.headers,
@@ -171,17 +169,19 @@ const Users = () => {
                   ${deleteButtonPressed && deleteButtonPressedOnUserId==row.id ? "bg-red-200" : "even:bg-gray-400"}
                 `}
               >
-                <td>{index+1+(paginationWantedPage-1)*paginationPageSize}</td>
+                <td>{index+1}</td>
                 {tableData.headers.map((header) => (
                   <td key={header.key}>
+                    {/* {(row as Row)[header.key as keyof Row]} */}
                     {row.hasOwnProperty(header.key) ? row[header.key as keyof Row] : 'N/A'}
                   </td>
                 ))}
                 <td
                   className="space-x-2"
                 >
+                  {/* <p>{row.id}</p> */}
                   <button
-                    className="w-10 h-8 bg-yellow-300 rounded"
+                    className="px-3 py-1 bg-yellow-300 rounded"
                     onClick={() => navigate(`${row.id}`)}
                   >
                     Edit
@@ -213,46 +213,20 @@ const Users = () => {
         <p className="font-bold">{loadingState}</p>
       }
 
-      {tableData.rows.length > 0 && 
-        <div
-          data-info="numbers-for-pages"
-          className="flex justify-center items-center gap-1 mt-3 mb-3"
-        >
-          {!paginationPageNumbers.includes(1) &&
-          (
-            <>
-              <button
-                onClick={() => setPaginationWantedPage(1)}
-                className="w-10 h-8 bg-gray-400 rounded-md mr-2"
-              >
-                {"1..."}
-              </button>
-            </>
-          )
-          }
-          {paginationPageNumbers.map((pageNumber) => (
-            <button
-              key={pageNumber}
-              onClick={() => setPaginationWantedPage(pageNumber)}
-              className={`${pageNumber === paginationWantedPage ? "font-bold cursor-default" : ""} w-10 h-8 bg-gray-400 rounded-md`}
-            >
-              {pageNumber}
-            </button>
-          ))}
-          {!paginationPageNumbers.includes(tableData.pagination_description.total_pages) &&
-            (
-              <>
-                <button
-                  onClick={() => setPaginationWantedPage(tableData.pagination_description.total_pages)}
-                  className="w-10 h-8 bg-gray-400 rounded-md ml-2"
-                >
-                  {"..." + tableData.pagination_description.total_pages}
-                </button>
-              </>
-            )
-          }
-        </div>
-      }
+      <div
+        data-info="numbers-for-pages"
+        className="flex justify-center items-center"
+      >
+        {/* {for (let index = 0; index < array.length; index++) {
+          const element = array[index];
+          
+        }} */}
+
+        Total pages: {tableData.pagination_description.total_pages}
+        {/* TODO: page n of m */}
+        {/* CONTINUE add pagination */}
+
+      </div>
     </>
   )
 }
