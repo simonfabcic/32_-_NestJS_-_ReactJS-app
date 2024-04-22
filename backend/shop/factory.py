31b8c0ddfdffe59fd7d42f0401de9208236de195
Simon Fabcic <simon.fabcic@gmail.com>
2024-04-22 19:59:04 +0200

    First test for: - core (urls, views) - shop (urls, views)

---
+++ backend/shop/factory.py
@@ -0,0 +1,12 @@
+import factory
+
+from shop.models import ShopProfile
+from core.factory import CoreUserFactory
+
+class ShopProfileFactory(factory.django.DjangoModelFactory):
+  class Meta:
+    model = ShopProfile
+
+  user = factory.SubFactory(CoreUserFactory)
+  first_name = factory.Faker("name")
+  last_name = factory.Faker("last_name")
