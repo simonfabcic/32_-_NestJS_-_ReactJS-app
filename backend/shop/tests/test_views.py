1d94a22cf2f8ce1eeade3b5a20667dbc976c8282
Simon Fabcic <simon.fabcic@gmail.com>
2024-04-25 10:48:09 +0200

    Minor changes in 'test_views' for shop Edited 'update_user' function in backend Fixed something on 'seed.py'

--- backend/shop/tests/test_views.py
+++ backend/shop/tests/test_views.py
@@ -19,29 +19,29 @@ class TestViews(TestCase):
         }
 
     def test_adding_new_profile_success(self):
-        no_of_shop_profiles_before = ShopProfile.objects.count()
+        no_of_shop_profiles_before = ShopProfile.objects.all().count()
         response = self.client.post(reverse("profile_new"), data=self.user_data)
         self.assertEqual(response.status_code, 201)
-        no_of_shop_profiles_after = ShopProfile.objects.count()
-        # self.assertEqual(ShopProfile.objects.count(), 1)
+        no_of_shop_profiles_after = ShopProfile.objects.all().count()
+        # self.assertEqual(ShopProfile.objects.all().count(), 1)
         self.assertEqual(no_of_shop_profiles_before + 1, no_of_shop_profiles_after)
 
     def test_adding_new_profile_failure_duplicated_email(self):
         CoreUserFactory(email=self.user_data["email"])
         response = self.client.post(reverse("profile_new"), data=self.user_data)
         self.assertEqual(response.status_code, 409)
-        # self.assertEqual(ShopProfile.objects.count(), 0)
+        # self.assertEqual(ShopProfile.objects.all().count(), 0)
         # JURE the line above is throwing an error - what is the issue:
         # django.db.transaction.TransactionManagementError: An error occurred in the current transaction. You can't execute queries until the end of the 'atomic' block.
 
     def test_adding_new_profile_failure_no_data(self):
         response = self.client.post(reverse("profile_new"))
         self.assertEqual(response.status_code, 422)
-        self.assertEqual(ShopProfile.objects.count(), 0)
+        self.assertEqual(ShopProfile.objects.all().count(), 0)
 
     def test_adding_new_profile_failure_missing_lastName(self):
         data_without_lastName = self.user_data.copy()
         del data_without_lastName["lastName"]
         response = self.client.post(reverse("profile_new"), data=data_without_lastName)
         self.assertEqual(response.status_code, 422)
-        self.assertEqual(ShopProfile.objects.count(), 0)
+        self.assertEqual(ShopProfile.objects.all().count(), 0)
