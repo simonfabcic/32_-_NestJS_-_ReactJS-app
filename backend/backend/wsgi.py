cd7af3c62089496103d0d34e6553968e59e98e0e
Simon Fabcic <simon.fabcic@gmail.com>
2024-05-05 14:32:22 +0200

    Add django-configurations package and use it to manage settings
    * Added `django-configurations` package to Pipfile and Pipfile.lock
    * Created a base settings class in settings.py and two subclasses for 'dev' and 'production'

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
