d4e42a0a09f3ee339eb380d195b0471d1070a9d2
Simon Fabcic <simon.fabcic@gmail.com>
2024-05-13 13:47:14 +0200

    Frontend - add `Roles.tsx`
    Added just basic `Roles` view.
    Getting data via API and show them.
    
    Tests came in future, when I figure out how to mock API request.

--- frontend/src/pages/dashboard/roles/Roles.tsx
+++ frontend/src/pages/dashboard/roles/Roles.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useAxios from "../../utils/useAxios"
 
 const Roles = () => {
+  // INTERFACES ---------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  interface Role {
+    name: string;
+  }
+
+  // VARIABLES ----------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  const [roles, setRoles] = useState<Role[]>([])
+  let api = useAxios()
+  let status = ""
+
+  // API REQUESTS -------------------------------------------------------------
+  // --------------------------------------------------------------------------
+
+  useEffect(() => {
+    getProfiles()
+  }, [])
+  let getProfiles = async () => {
+    try {
+      let response = await api.get((`/shop-api-v1/roles`))
+      console.log(response)
+      if (response.status === 200) {
+        setRoles(() => response.data)
+        if (response.data.rows.length < 1)
+          status = "Didn't receive any roles. Nothing to show."
+      }
+    } catch (err: any) {
+      console.error("During getting 'Roles', err occurred: ", err.message, "\nMessage from server:", err.response?.data);
+      status = "Can't get profiles data. Contact admin..."
+    }
+  }
+
   return (
-    <div>Roles</div>
+    <>
+      <div>Roles</div>
+      {roles && <div>Test</div>}
+      {roles &&  roles.map((role, index) => (
+        <div key={index}>{role.name}</div>
+      )) }
+    </>
   )
 }
 
