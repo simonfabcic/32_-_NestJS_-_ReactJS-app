545a4a6c4fa2d1495c8a9803178a9ded2e5f22b6
Simon Fabcic <simon.fabcic@gmail.com>
2024-05-14 08:35:32 +0200

    Added basic test with mocking the `Roles`
    [Why?]
    To figure out how to mock response from backend.
    
    [How?]
    Using `axios-mock-adapter` im simulating the backend response and return `roles`.
    
    [Testing?]
    Test if mocked `roles` are presented in component.

---
+++ frontend/src/pages/dashboard/roles/Roles.test.tsx
@@ -0,0 +1,30 @@
+import MockAdapter from "axios-mock-adapter";
+import { render, waitFor, screen } from "@testing-library/react";
+import { describe, test, expect } from "vitest";
+import axios from "axios";
+
+import Roles from "./Roles";
+
+const mock = new MockAdapter(axios, { onNoMatch: "throwException" });
+
+const roles = [
+    { name: "Administrator" },
+    { name: "User" },
+    { name: "Manager" },
+];
+
+describe("axios mocking test", () => {
+    test("should render loading followed by roles", async () => {
+        mock.onGet("/shop-api-v1/roles").reply(200, roles);
+
+        const wrapper = render(<Roles />);
+        expect(wrapper).toBeTruthy();
+
+        await waitFor(() => {
+            roles.forEach((role) => {
+                const text = screen.getByText(role.name);
+                expect(text.textContent).toBeTruthy();
+            });
+        });
+    });
+});
