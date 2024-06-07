58fcc1c62287fe1dfc24a44b052ea2e009c8ed01
Simon Fabcic <simon.fabcic@gmail.com>
2024-06-07 08:00:12 +0200

    Rewrite tests from `TestCase` to `APITestCase`

--- backend/shop/tests/old_test_views_with_testcase.py
+++ backend/shop/tests/old_test_views_with_testcase.py
@@ -1,18 +1,17 @@
-from rest_framework.test import APITestCase, APIClient
-from django.contrib.auth.models import Permission, Group
+from django.test import TestCase, Client
 from django.urls import reverse
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 import json
+from django.contrib.auth.models import Permission, Group
 
 from shop.models import ShopProfile
-from core.factory import CoreUserFactory
 from shop.factory import ShopProfileFactory
+from core.factory import CoreUserFactory
 
 
-class TestProfile(APITestCase):
+class TestProfile(TestCase):
 
     def setUp(self):
-        self.client = APIClient()
-
         self.user_data = {
             "firstName": "Jane",
             "lastName": "Doe",
@@ -20,12 +19,21 @@ class TestProfile(APITestCase):
             "password": "secure_password",
             "avatar": "/dummy_image.com/259x267",
         }
+        self.user = CoreUserFactory()
+
+        self.client_with_access_token = Client()
+        refresh_token = RefreshToken.for_user(self.user)
+        headers = {"Authorization": f"Bearer {refresh_token.access_token}"}
+        self.client_with_access_token.defaults["HTTP_AUTHORIZATION"] = headers[
+            "Authorization"
+        ]
 
     def test_adding_new_profile_success(self):
         no_of_shop_profiles_before = ShopProfile.objects.all().count()
         response = self.client.post(reverse("profile_create"), data=self.user_data)
         self.assertEqual(response.status_code, 201)
         no_of_shop_profiles_after = ShopProfile.objects.all().count()
+        # self.assertEqual(ShopProfile.objects.all().count(), 1)
         self.assertEqual(no_of_shop_profiles_before + 1, no_of_shop_profiles_after)
 
     def test_adding_new_profile_failure_duplicated_email(self):
@@ -33,6 +41,8 @@ class TestProfile(APITestCase):
         response = self.client.post(reverse("profile_create"), data=self.user_data)
         self.assertEqual(response.status_code, 409)
         # self.assertEqual(ShopProfile.objects.all().count(), 0)
+        # JURE the line above is throwing an error - what is the issue:
+        # django.db.transaction.TransactionManagementError: An error occurred in the current transaction. You can't execute queries until the end of the 'atomic' block.
 
     def test_adding_new_profile_failure_no_data(self):
         response = self.client.post(reverse("profile_create"))
@@ -50,12 +60,10 @@ class TestProfile(APITestCase):
 
     def test_get_profile_success(self):
         shop_profile = ShopProfileFactory()
-        self.client.force_authenticate(user=shop_profile.user)
-
         self.assertEqual(ShopProfile.objects.all().count(), 1)
 
         url = reverse("profile", kwargs={"profile_id": shop_profile.id})
-        response = self.client.get(url)
+        response = self.client_with_access_token.get(url)
 
         self.assertEqual(response.status_code, 200)
 
@@ -69,59 +77,56 @@ class TestProfile(APITestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_shop_profile_update_all_success(self):
-        shop_profile = ShopProfileFactory()
-        shop_profile_id = shop_profile.id
-        self.client.force_authenticate(user=shop_profile.user)
-        url = reverse("profile", kwargs={"profile_id": shop_profile_id})
+        profile = ShopProfileFactory()
+        profile_id = profile.id
+        url = reverse("profile", kwargs={"profile_id": profile_id})
         data_update = {
             "firstName": "Alice",
             "lastName": "Smith",
             "email": "alice@example.com",
             "password": "password",
-            "userID": shop_profile_id,
+            "userID": profile_id,
         }
         data = json.dumps(data_update)
-        response = self.client.put(
+        response = self.client_with_access_token.put(
             url,
             data,
             content_type="application/json",
         )
         self.assertEqual(response.status_code, 204)
 
-        profile_after = ShopProfile.objects.get(id=shop_profile_id)
-        self.assertEqual(shop_profile.user, profile_after.user)
+        profile_after = ShopProfile.objects.get(id=profile_id)
+        self.assertEqual(profile.user, profile_after.user)
         self.assertEqual(profile_after.user.email, data_update["email"])
         self.assertEqual(profile_after.first_name, data_update["firstName"])
         self.assertEqual(profile_after.last_name, data_update["lastName"])
-        self.assertNotEqual(shop_profile.user.password, profile_after.user.password)
+        self.assertNotEqual(profile.user.password, profile_after.user.password)
 
     def test_shop_profile_update_only_first_name(self):
-        shop_profile = ShopProfileFactory()
-        self.client.force_authenticate(user=shop_profile.user)
-        shop_profile_id = shop_profile.id
-        url = reverse("profile", kwargs={"profile_id": shop_profile_id})
+        profile = ShopProfileFactory()
+        profile_id = profile.id
+        url = reverse("profile", kwargs={"profile_id": profile_id})
         data_update = {
             "firstName": "John-changed",
-            "userID": shop_profile_id,
+            "userID": profile_id,
         }
         data = json.dumps(data_update)
-        response = self.client.put(
+        response = self.client_with_access_token.put(
             url,
             data,
             content_type="application/json",
         )
         self.assertEqual(response.status_code, 204)
 
-        profile_after = ShopProfile.objects.get(id=shop_profile_id)
-        self.assertEqual(shop_profile.user, profile_after.user)
-        self.assertEqual(shop_profile.user.email, profile_after.user.email)
+        profile_after = ShopProfile.objects.get(id=profile_id)
+        self.assertEqual(profile.user, profile_after.user)
+        self.assertEqual(profile.user.email, profile_after.user.email)
         self.assertEqual(profile_after.first_name, data_update["firstName"])
-        self.assertEqual(shop_profile.last_name, profile_after.last_name)
-        self.assertEqual(shop_profile.user.password, profile_after.user.password)
+        self.assertEqual(profile.last_name, profile_after.last_name)
+        self.assertEqual(profile.user.password, profile_after.user.password)
 
     def test_shop_profile_create_if_not_exists_success(self):
         user = CoreUserFactory()
-        self.client.force_authenticate(user=user)
         no_of_profiles = ShopProfile.objects.all().count()
 
         url = reverse("profile", kwargs={"profile_id": "null"})
@@ -132,7 +137,7 @@ class TestProfile(APITestCase):
             "userID": "null",
         }
         data = json.dumps(data_update)
-        response = self.client.put(
+        response = self.client_with_access_token.put(
             url,
             data,
             content_type="application/json",
@@ -149,7 +154,7 @@ class TestProfile(APITestCase):
         self.assertEqual(profile.first_name, data_update["firstName"])
         self.assertEqual(profile.last_name, data_update["lastName"])
 
-    def test_shop_profile_update_failure_not_authenticated(self):
+    def test_shop_profile_update_failure_not_signed_in(self):
         url = reverse("profile", kwargs={"profile_id": 1})
         response = self.client.put(
             url,
@@ -157,18 +162,30 @@ class TestProfile(APITestCase):
         self.assertEqual(response.status_code, 302)
 
 
-class TestAvatar(APITestCase):
+class TestAvatar(TestCase):
 
     def setUp(self):
-        self.client = APIClient()
+        self.user_data = {
+            "firstName": "Jane",
+            "lastName": "Doe",
+            "email": "existing@example.com",
+            "password": "secure_password",
+            "avatar": "/dummy_image.com/259x267",
+        }
+        self.user = CoreUserFactory()
 
-    def test_shop_profile_success_get_img_url(self):
+        self.client_with_access_token = Client()
+        refresh_token = RefreshToken.for_user(self.user)
+        headers = {"Authorization": f"Bearer {refresh_token.access_token}"}
+        self.client_with_access_token.defaults["HTTP_AUTHORIZATION"] = headers[
+            "Authorization"
+        ]
+
+    def test_shop_profile_get_img_url(self):
         shop_profile = ShopProfileFactory()
 
-        self.client.force_authenticate(user=shop_profile.user)
-
         url = reverse("profile", kwargs={"profile_id": shop_profile.id})
-        response = self.client.get(url)
+        response = self.client_with_access_token.get(url)
         self.assertEqual(response.status_code, 200)
 
         avatar_response = response.json()["avatar"]
@@ -178,44 +195,40 @@ class TestAvatar(APITestCase):
     def test_shop_profile_no_img_url(self):
         shop_profile = ShopProfileFactory(avatar=None)
 
-        self.client.force_authenticate(user=shop_profile.user)
-
         url = reverse("profile", kwargs={"profile_id": shop_profile.id})
-        response = self.client.get(url)
+        response = self.client_with_access_token.get(url)
         self.assertEqual(response.status_code, 200)
 
         avatar_response = response.json()["avatar"]
         self.assertEqual(avatar_response, None)
 
     def test_profile_contain_avatar_success(self):
-        user_data = {
-            "firstName": "Jane",
-            "lastName": "Doe",
-            "email": "existing@example.com",
-            "password": "secure_password",
-            "avatar": "/dummy_image.com/259x267",
-        }
-
-        response = self.client.post(reverse("profile_create"), data=user_data)
+        response = self.client.post(reverse("profile_create"), data=self.user_data)
         self.assertEqual(response.status_code, 201)
 
-        profile = ShopProfile.objects.get(user__email=user_data["email"])
-        self.assertEqual(profile.avatar, user_data["avatar"])
+        profile = ShopProfile.objects.get(user__email=self.user_data["email"])
+        self.assertEqual(profile.avatar, self.user_data["avatar"])
 
 
-class TestPermission(APITestCase):
+class TestPermission(TestCase):
+
     def setUp(self):
-        self.client = APIClient()
         self.user = CoreUserFactory()
 
-    def test_permissions_get_success(self):
-        permission_view_role = Permission.objects.get(codename="change_role")
-        self.user.user_permissions.add(permission_view_role)
-        self.client.force_authenticate(user=self.user)
+        self.client_with_access_token = Client()
+        refresh_token = RefreshToken.for_user(self.user)
+        headers = {"Authorization": f"Bearer {refresh_token.access_token}"}
+        self.client_with_access_token.defaults["HTTP_AUTHORIZATION"] = headers[
+            "Authorization"
+        ]
 
+        self.permission_change_role = Permission.objects.get(codename="change_role")
+        self.permission_view_role = Permission.objects.get(codename="view_role")
+
+    def test_returning_permissions_success_permission_change_role(self):
         url = reverse("permission_get")
-
-        response = self.client.get(url)
+        self.user.user_permissions.add(self.permission_change_role)
+        response = self.client_with_access_token.get(url)
         self.assertEqual(response.status_code, 200)
         returned_codenames = {permission["codename"] for permission in response.data}
         expected_codenames = {
@@ -226,132 +239,109 @@ class TestPermission(APITestCase):
         }
         self.assertTrue(expected_codenames.issubset(returned_codenames))
 
-    def test_permission_get_failure_authenticated_no_permission(self):
-        self.client.force_authenticate(user=self.user)
-
+    def test_returning_permissions_failure_not_logged_in(self):
         url = reverse("permission_get")
-
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 403)
-
-    def test_permission_get_failure_not_authenticated(self):
-        url = reverse("permission_get")
-
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
 
-    def test_permission_get_failure_authenticated_wrong_permission(self):
-        permission_view_role = Permission.objects.get(codename="view_shopprofile")
-        self.user.user_permissions.add(permission_view_role)
-        self.client.force_authenticate(user=self.user)
-
+    def test_returning_permissions_failure_logged_in_no_rights(self):
         url = reverse("permission_get")
+        response = self.client_with_access_token.get(url)
+        self.assertEqual(response.status_code, 403)
 
-        response = self.client.get(url)
+    def test_returning_permissions_failure_wrong_permission(self):
+        url = reverse("permission_get")
+        self.user.user_permissions.add(self.permission_view_role)
+        response = self.client_with_access_token.get(url)
         self.assertEqual(response.status_code, 403)
 
 
-class TestGroup(APITestCase):
+class TestGroup(TestCase):
 
     def setUp(self):
-        self.client = APIClient()
         self.user = CoreUserFactory()
 
+        self.client_with_access_token = Client()
+        refresh_token = RefreshToken.for_user(self.user)
+        headers = {"Authorization": f"Bearer {refresh_token.access_token}"}
+        self.client_with_access_token.defaults["HTTP_AUTHORIZATION"] = headers[
+            "Authorization"
+        ]
+
         self.permission_change_role = Permission.objects.get(codename="change_role")
+        self.permission_view_role = Permission.objects.get(codename="view_role")
+        self.group = Group.objects.create(name="test_group")
 
     # GET role
-    def test_role_get_success(self):
+
+    def test_role_get_all_success_view_role_permission(self):
         url = reverse("role_get")
-
-        permission_view_role = Permission.objects.get(codename="view_role")
-        self.user.user_permissions.add(permission_view_role)
-        self.client.force_authenticate(user=self.user)
-
-        response = self.client.get(url)
+        self.user.user_permissions.add(self.permission_view_role)
+        response = self.client_with_access_token.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_role_get_failure_not_authenticated(self):
+    def test_role_get_failure_not_logged_in(self):
         url = reverse("role_get")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
 
-    def test_role_get_failure_authenticated_no_permission(self):
+    def test_role_get_failure_logged_in_no_permission(self):
         url = reverse("role_get")
-
-        self.client.force_authenticate(user=self.user)
-
-        response = self.client.get(url)
+        response = self.client_with_access_token.get(url)
         self.assertEqual(response.status_code, 403)
 
-    def test_role_get_failure_authenticated_wrong_permission(self):
+    def test_role_get_failure_wrong_permission(self):
         url = reverse("role_get")
-
-        permission_change_role = Permission.objects.get(codename="change_role")
-        self.user.user_permissions.add(permission_change_role)
-        self.client.force_authenticate(user=self.user)
-
-        response = self.client.get(url)
+        self.user.user_permissions.add(self.permission_change_role)
+        response = self.client_with_access_token.get(url)
         self.assertEqual(response.status_code, 403)
 
     # GET permission
-    def test_permissions_get_success_permission_in_group(self):
+    def test_returning_permissions_success_permission_in_group(self):
         url = reverse("permission_get")
-
-        group = Group.objects.create(name="test_group")
-        permission_change_role = Permission.objects.get(codename="change_role")
-        group.permissions.add(permission_change_role)
-        self.user.groups.add(group)
-        self.client.force_authenticate(user=self.user)
-
-        response = self.client.get(url)
+        self.group.permissions.add(self.permission_change_role)
+        self.user.groups.add(self.group)
+        response = self.client_with_access_token.get(url)
         self.assertEqual(response.status_code, 200)
 
     # PUT role
     def test_role_put_success(self):
-        url = reverse("role_create")
-
-        permission_change_role = Permission.objects.get(codename="change_role")
-        self.user.user_permissions.add(permission_change_role)
-        self.client.force_authenticate(user=self.user)
-
         no_of_groups_before = Group.objects.all().count()
-
+        url = reverse("role_create")
+        self.user.user_permissions.add(self.permission_change_role)
         data = {"name": "role_name"}
-        response = self.client.put(
+        response = self.client_with_access_token.put(
             url,
             data,
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, 201)
 
         no_of_groups_after = Group.objects.all().count()
         self.assertEqual(no_of_groups_before + 1, no_of_groups_after)
 
-    def test_role_put_failure_not_authenticated(self):
-        url = reverse("role_create")
-
+    def test_role_put_failure_not_logged_in(self):
         no_of_groups_before = Group.objects.all().count()
-
+        url = reverse("role_create")
         data = {"name": "role_name"}
         response = self.client.put(
             url,
             data,
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, 302)
 
         no_of_groups_after = Group.objects.all().count()
         self.assertEqual(no_of_groups_before, no_of_groups_after)
 
-    def test_role_put_failure_authenticated_no_permission(self):
-        url = reverse("role_create")
-
-        self.client.force_authenticate(user=self.user)
-
+    def test_role_put_failure_logged_in_no_permission(self):
         no_of_groups_before = Group.objects.all().count()
-
+        url = reverse("role_create")
         data = {"name": "role_name"}
-        response = self.client.put(
+        response = self.client_with_access_token.put(
             url,
             data,
+            content_type="application/json",
         )
         self.assertEqual(response.status_code, 403)
 
