69ea7566e4b7cc4f9fefc0f809c7be4a092f9f81
Simon Fabcic <simon.fabcic@gmail.com>
2024-06-13 12:40:59 +0200

    .

--- backend/manage.py
+++ backend/manage.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
 
+    # uncomment this two lines, if you want to use `python manage.py shell`
     # from configurations.management import execute_from_command_line
-
     # execute_from_command_line(sys.argv)
