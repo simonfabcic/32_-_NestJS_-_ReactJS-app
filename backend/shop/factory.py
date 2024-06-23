3f6af4624fc222f1f4eaebc02aedeb29a4a02c27
Simon Fabcic <simon.fabcic@gmail.com>
2024-06-23 13:49:01 +0200

    Changes according to previous PR

--- backend/shop/factory.py
+++ backend/shop/factory.py
@@ -1,5 +1,9 @@
+from io import BytesIO
+
 import factory
 from core.factory import CoreUserFactory
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
 from shop.models import Product, ShopProfile
 
 
@@ -10,14 +14,33 @@ class ShopProfileFactory(factory.django.DjangoModelFactory):
     user = factory.SubFactory(CoreUserFactory)
     first_name = factory.Faker("name")
     last_name = factory.Faker("last_name")
-    avatar = factory.Faker("image_url")
+
+    @factory.lazy_attribute
+    def avatar(self):
+        binary_stream = BytesIO()
+        Image.new("RGB", (100, 100)).save(binary_stream, format="JPEG")
+        binary_stream.seek(0)
+        return SimpleUploadedFile(
+            "test_avatar.jpg", binary_stream.read(), content_type="image/jpeg"
+        )
 
 
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Product
 
-    image = factory.Faker("image_url")
+    @factory.lazy_attribute
+    def image(self):
+        # create an in-memory image
+        binary_stream = BytesIO()
+        Image.new("RGB", (100, 100)).save(binary_stream, format="JPEG")
+        # sets the file pointer to the beginning of the BytesIO object
+        binary_stream.seek(0)
+        return SimpleUploadedFile(
+            "test_image.jpg", binary_stream.read(), content_type="image/jpeg"
+        )
+
+    # image = factory.Faker("image_url")
     title = factory.Faker("word")
     description = factory.Faker("sentence")
     price = factory.Faker(
--- backend/shop/models.py
+++ backend/shop/models.py
@@ -17,7 +17,7 @@ class Order(models.Model):
 class Product(models.Model):
     image = models.ImageField(blank=True, default="", upload_to="images/products")
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
 
